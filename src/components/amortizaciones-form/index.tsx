6173c64463a822acc2cb572f30754590efbc784c
Rafael <rafael0868@gmail.com>
2023-10-05 20:33:48 -0600

    segundo cambio 5/10

--- src/components/amortizaciones-form/index.tsx
+++ src/components/amortizaciones-form/index.tsx
@@ -57,110 +57,110 @@ const AmortizacionesForm: React.FC<any> = ({ handleSubmit }) => {
 
   return (
     <form onSubmit={handleSubmit(formulario)}>
-      
+
       <Grid container columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
         <Grid item xs={6}>
-        <TextField required sx={{ mb: 2}} label="Codigo" variant="outlined" id="outlined-disabled"/>
+          <TextField required sx={{ mb: 2 }} label="Codigo" variant="outlined" id="outlined-disabled" />
         </Grid>
         <Grid item xs={6}>
-        <FormControl fullWidth sx={{ mb: 2, minWidth: 120}} required>
-        <InputLabel id="demo-simple-select-label">Factura</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="codFactura"
-          value={formulario?.codFactura}
-          label="Factura"
-          name="codFactura"
-          onChange={handleSelectedChange}
-          margin="none"
-          autoWidth
-        >
-          <MenuItem value={10}>12457893</MenuItem>
-          <MenuItem value={20}>33654788</MenuItem>
-          <MenuItem value={30}>45487952</MenuItem>
-        </Select>
-      </FormControl>
+          <FormControl fullWidth sx={{ mb: 2, minWidth: 120 }} required>
+            <InputLabel id="demo-simple-select-label">Factura</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="codFactura"
+              value={formulario?.codFactura}
+              label="Factura"
+              name="codFactura"
+              onChange={handleSelectedChange}
+              margin="none"
+              autoWidth
+            >
+              <MenuItem value={10}>12457893</MenuItem>
+              <MenuItem value={20}>33654788</MenuItem>
+              <MenuItem value={30}>45487952</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
-      </Grid>   
-      
-      
-      <TextField helperText="Nombre del Cliente" fullWidth sx={{ mb: 1,}} label="" variant="outlined" disabled id="outlined-disabled"/>
-      
+      </Grid>
+
+
+      <TextField helperText="Nombre del Cliente" fullWidth sx={{ mb: 1, }} label="Ejemplo" type="text" variant="outlined" disabled value={"Example"} id="outlined-basic" />
+
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
         <Grid item xs={6}>
-        <TextField helperText="Saldo Pendiente" fullWidth sx={{ mb: 1,}} label="Q150,000.00" variant="outlined" disabled id="outlined-disabled"/>
+          <TextField helperText="Saldo Pendiente" fullWidth sx={{ mb: 1, }} label="Q150,000.00" variant="outlined" disabled id="outlined-disabled" />
         </Grid>
         <Grid item xs={6}>
-        <TextField helperText="Fecha Limite" fullWidth sx={{ mb: 1,}} label="DD/MM/AA" variant="outlined" disabled id="outlined-disabled"/>
+          <TextField helperText="Fecha Limite" fullWidth sx={{ mb: 1, }} label="DD/MM/AA" variant="outlined" disabled id="outlined-disabled" />
         </Grid>
       </Grid>
 
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
         <Grid item xs={6}>
-        <TextField helperText="Intereses" fullWidth sx={{ mb: 1,}} label="Q50.00" variant="outlined" disabled id="outlined-disabled"/>
+          <TextField helperText="Intereses" fullWidth sx={{ mb: 1, }} label="Q50.00" variant="outlined" disabled id="outlined-disabled" />
         </Grid>
         <Grid item xs={6}>
-        <TextField helperText="Total a Pagar" fullWidth sx={{ mb: 1,}} label="Q150,050.00" variant="outlined" disabled id="outlined-disabled"/>
+          <TextField helperText="Total a Pagar" fullWidth sx={{ mb: 1, }} label="Q150,050.00" variant="outlined" disabled id="outlined-disabled" />
         </Grid>
-      </Grid> 
+      </Grid>
 
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
         <Grid item xs={6}>
-        <center><Button onClick={handleClickOpen} type="submit" variant="contained" color="success" startIcon={<CheckCircleIcon/>}>
-          ACEPTAR
-        </Button>
-        </center>
+          <center><Button onClick={handleClickOpen} type="submit" variant="contained" color="success" startIcon={<CheckCircleIcon />}>
+            ACEPTAR
+          </Button>
+          </center>
         </Grid>
         <Grid item xs={6}>
-        <center><Button  type="button" variant="contained" color="error" startIcon={<DeleteRoundedIcon/>}>
-        LIMPIAR
-        </Button>
-        </center>
+          <center><Button type="button" variant="contained" color="error" startIcon={<DeleteRoundedIcon />}>
+            LIMPIAR
+          </Button>
+          </center>
         </Grid>
-      </Grid> 
+      </Grid>
 
       <Dialog open={open} onClose={handleClose} fullWidth>
         <DialogTitle>Amortizaciones</DialogTitle>
         <DialogContent dividers>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-        <Grid item xs={6}>
-        <TextField fullWidth required sx={{ mb: 2}} label="# Recibo" variant="outlined" id="outlined-disabled"/>
-        </Grid>
-        <Grid item xs={6}>
-        <FormControl fullWidth sx={{ mb: 2, minWidth: 120}} required>
-        <InputLabel id="demo-simple-select-label">Metodo Pago</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          label="Factura"
-          name="codFactura"
-          onChange={handleSelectedChange}
-          margin="none"
-          autoWidth        >
-          <MenuItem value={10}>Transferencia</MenuItem>
-          <MenuItem value={20}>Efectivo</MenuItem>
-        </Select>
-      </FormControl>
-        </Grid>
-      </Grid>
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+            <Grid item xs={6}>
+              <TextField fullWidth required sx={{ mb: 2 }} label="# Recibo" variant="outlined" id="outlined-disabled" />
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth sx={{ mb: 2, minWidth: 120 }} required>
+                <InputLabel id="demo-simple-select-label">Metodo Pago</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  label="Factura"
+                  name="codFactura"
+                  onChange={handleSelectedChange}
+                  margin="none"
+                  autoWidth        >
+                  <MenuItem value={10}>Transferencia</MenuItem>
+                  <MenuItem value={20}>Efectivo</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
 
-      <TextField helperText="Descripción" fullWidth sx={{ mb: 1,}} label="Descripción" variant="outlined" id="outlined-disabled" required/>
-      
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-        <Grid item xs={6}>
-        <TextField helperText="Numero de Transferencia" fullWidth sx={{ mb: 1,}} label="#" variant="outlined" id="outlined-disabled"/>
-        </Grid>
-        <Grid item xs={6}>
-        <TextField helperText="Monto" fullWidth sx={{ mb: 1,}} label="Q" variant="outlined" id="outlined-disabled" required/>
-        </Grid>
-      </Grid>       
+          <TextField helperText="Descripción" fullWidth sx={{ mb: 1, }} label="Descripción" variant="outlined" id="outlined-disabled" required />
+
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+            <Grid item xs={6}>
+              <TextField helperText="Numero de Transferencia" fullWidth sx={{ mb: 1, }} label="#" variant="outlined" id="outlined-disabled" />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField helperText="Monto" fullWidth sx={{ mb: 1, }} label="Q" variant="outlined" id="outlined-disabled" required />
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} variant="contained" color="error" startIcon={<CancelRoundedIcon/>} sx={{maxWidth: 115}}>CANCELAR</Button>
-          <Button variant="contained" color="success" startIcon={<CheckCircleIcon/>}>GRABAR</Button>
+          <Button onClick={handleClose} variant="contained" color="error" startIcon={<CancelRoundedIcon />} sx={{ maxWidth: 115 }}>CANCELAR</Button>
+          <Button variant="contained" color="success" startIcon={<CheckCircleIcon />}>GRABAR</Button>
         </DialogActions>
       </Dialog>
     </form>
-    
+
   );
 };
 
